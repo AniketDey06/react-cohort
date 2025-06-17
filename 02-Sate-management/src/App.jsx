d9a258f6ecf812d88e32223b925c3c37ad185926
Aniket Dey <meaniket1025@gmail.com>
2025-06-17 13:14:28 +0530

    feat: integrate Tailwind CSS and update App component
    - Added Tailwind CSS, Autoprefixer, and PostCSS to package.json.
    - Updated App component to manage todos and conditionally render a button.
    - Implemented basic styling for todos using Tailwind CSS.
    - Created postcss.config.js and tailwind.config.js for Tailwind setup.
    - Updated index.css to include Tailwind directives.

--- 02-Sate-management/src/App.jsx
+++ 02-Sate-management/src/App.jsx
@@ -26,23 +26,65 @@ function App() {
   //   }
   // }, [])
 
+
+
   let a = useRef(0)
 
   useEffect(() => {
     a.current = a.current + 1
     console.log(`fgnfjngvjfnv${a.current}`);
-    
+
   })
-  
+
+  const [showBtn, setshowBtn] = useState(false)
+
+  const [todos, setTodos] = useState([
+    {
+      title: "todo 1",
+      desc: "desc 1",
+    },
+    {
+      title: "todo 2",
+      desc: "desc 2",
+    },
+    {
+      title: "todo 3",
+      desc: "desc 3",
+    },
+  ])
+
+  // const Todo = ({ todo }) => {
+
+  //   return (
+  //     <>
+  //       <div className='m-4 border border-1 border-purple-400'>
+  //         <div className="todo">{todo.title}</div>
+  //         <div className="todo">{todo.desc}</div>
+  //       </div>
+  //     </>
+  //   )
+  // }
 
 
   return (
     <>
+      {showBtn && <button onClick={() => { setCount(count + 1) }}>Click</button>}
       <Navbar color={"navy" + "blue"} />
       <div>This Count is : {count}</div>
 
-      <button onClick={() => { setCount(count + 1) }}>Click</button>
+      {/* <button onClick={() => { setCount(count + 1) }}>Click</button> */}
+      <button onClick={() => { setshowBtn(!showBtn) }}>Click</button>
 
+      {todos.map(todo => {
+        // return <Todo key={todo.title} todo={todo} />
+          return (
+            <div className='m-4 border border-1 border-purple-400' key={todo.title}>
+              <div className="todo">{todo.title}</div>
+              <div className="todo">{todo.desc}</div>
+            </div>
+          )
+        }
+      )}
 
 
     </>
