1803a606b7c1aa24455807f98a4862caa7d9cdc2
Aniket Dey <meaniket1025@gmail.com>
2025-06-19 20:47:25 +0530

    chore: update dependencies and remove unused configurations
    - Added @tailwindcss/vite dependency to package.json.
    - Removed postcss.config.js as it was no longer needed.
    - Added a new CSS class in App.css for a red square div.
    - Commented out unused state and effect hooks in App.jsx for cleaner code.
    - Removed tailwind.config.js as it was not required.

--- 02-Sate-management/src/App.jsx
+++ 02-Sate-management/src/App.jsx
@@ -7,7 +7,7 @@ import { data } from 'autoprefixer'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
   // const [color, setColor] = useState(0)
 
   // // useEffect(() => {
@@ -31,30 +31,30 @@ function App() {
 
 
 
-  let a = useRef(0)
+  // let a = useRef(0)
 
-  useEffect(() => {
-    a.current = a.current + 1
-    console.log(`fgnfjngvjfnv${a.current}`);
+  // useEffect(() => {
+  //   a.current = a.current + 1
+  //   console.log(`fgnfjngvjfnv${a.current}`);
 
-  })
+  // })
 
-  const [showBtn, setshowBtn] = useState(false)
+  // const [showBtn, setshowBtn] = useState(false)
 
-  const [todos, setTodos] = useState([
-    {
-      title: "todo 1",
-      desc: "desc 1",
-    },
-    {
-      title: "todo 2",
-      desc: "desc 2",
-    },
-    {
-      title: "todo 3",
-      desc: "desc 3",
-    },
-  ])
+  // const [todos, setTodos] = useState([
+  //   {
+  //     title: "todo 1",
+  //     desc: "desc 1",
+  //   },
+  //   {
+  //     title: "todo 2",
+  //     desc: "desc 2",
+  //   },
+  //   {
+  //     title: "todo 3",
+  //     desc: "desc 3",
+  //   },
+  // ])
 
   // const Todo = ({ todo }) => {
 
@@ -68,15 +68,49 @@ function App() {
   //   )
   // }
 
+  // const [name, setName] = useState("")
+  const [from, setFrom] = useState({name: "", phone: ""})
+
+  const handlemouse = () => {
+    // alert("mouse in")
+    const [color, setColor] = useState()
+  }
+
+  const [count, setCount] = useState(0)
+
+  const addValue = () => {
+    if(count>=5) return
+    setCount(count + 1)
+  }
+
+  const subValue = () => {
+    if(count<= -5) return
+    setCount(count-1)
+  }
+
+  const handleChange = (e) => {
+    // setName(e.target.value)
+    setFrom({...from, [e.target.name]:e.target.value})
+
+  }
 
   return (
     <>
+      <div>{count}</div>
+      <button onClick={addValue}>++ {count}</button>
+      <button onClick={subValue}>-- {count}</button>
+      {/* <div className='div' onMouseOver={handlemouse}>Click</div> */}
+      {/* <input type="text" value={name} onChange={handleChange}/> */}
+      {/* <input type="text" name="name" value={from.name} onChange={handleChange}/>
+      <input type="text" name="password" value={from.password} onChange={handleChange}/> */}
+
+
       {/* {showBtn && <button onClick={() => { setCount(count + 1) }}>Click</button>}
       <Navbar color={"navy" + "blue"} />
       <div>This Count is : {count}</div> */}
 
       {/* <button onClick={() => { setCount(count + 1) }}>Click</button> */}
-      <button onClick={() => { setshowBtn(!showBtn) }}>Click</button>
+      {/* <button onClick={() => { setshowBtn(!showBtn) }}>Click</button> */}
 
       {/* {todos.map(todo => {
         // return <Todo key={todo.title} todo={todo} />
