d6c3c2e9b8686ea178fba8a5a0c1601a6df34375
Aniket Dey <meaniket1025@gmail.com>
2025-06-28 20:50:56 +0530

    feat: initialize React Router project with Vite setup
    - Added package.json with dependencies for React, React DOM, and React Router DOM.
    - Included Vite configuration for development and build processes.
    - Created main application structure with App component and routing setup.
    - Implemented Navbar component for navigation between Home, About, and Dashboard.
    - Developed Home, About, and Dashboard components with basic content.
    - Styled application with CSS for layout and responsiveness.
    - Added SVG assets for branding (React and Vite logos).

---
+++ 05-React Router/src/App.jsx
@@ -0,0 +1,47 @@
+import './App.css'
+
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from './components/Home';
+import About from './components/About';
+import Dashboard from './components/Dashboard';
+import Navbar from './components/Navbar';
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element:
+        <div>
+          <Navbar />
+          <Home />
+        </div>,
+    },
+    {
+      path: '/about',
+      element:
+        <div>
+          <Navbar />
+          <About />
+        </div>,
+    },
+    {
+      path: '/dashboard',
+      element:
+        <div>
+          <Navbar />
+          <Dashboard />
+        </div>,
+    },
+  ]
+);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+      {/* <Navbar /> */}
+    </>
+  )
+}
+
+export default App
---
+++ 05-React Router/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import { Link, NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <NavLink to='/' className={({isActive}) => isActive ? "active-link" : ""}>
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/about'>
+                        About
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/dashboard'>
+                        Dashboard
+                    </NavLink>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
