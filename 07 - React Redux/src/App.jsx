ae49311ea1c385a96b6e53a7a412c1be6865c730
Aniket Dey <meaniket1025@gmail.com>
2025-07-04 20:14:33 +0530

    feat: implement counter functionality with Redux, including increment, decrement, reset, and increment by amount actions

--- 07 - React Redux/src/App.jsx
+++ 07 - React Redux/src/App.jsx
@@ -1,13 +1,46 @@
 import { useState } from 'react'
 
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, incrementByAmount, reset } from './features/counter/counterSlice'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0)
+
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  function handleIncrementClick() {
+    dispatch(increment())
+  }
+
+  function handleDecrementClick() {
+    dispatch(decrement())
+  }
+
+  function handleResetClick() {
+    dispatch(reset())
+  }
+
+  function handleIncByAmountClick(){
+    dispatch(incrementByAmount(amount))
+  }
 
   return (
     <>
-      hii
+      <button onClick={handleIncrementClick}>+</button>
+      <p>Count: {count}</p>
+      <button onClick={handleDecrementClick}>-</button>
+      <br />
+      <button onClick={handleResetClick}>Reset</button>
+      <br />
+      <input
+        type="number"
+        value={amount}
+        placeholder='Enter Amount'
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <button onClick={handleIncByAmountClick}>Inc by Amount</button>
     </>
   )
 }
