58f36400965e52221838ac29de6d56d50ba4d75f
Aniket Dey <meaniket1025@gmail.com>
2025-07-06 17:24:04 +0530

    feat: implement todo slice with add and remove functionality

--- 08 - React Redux 2/src/features/todo/todoSlice.js
+++ 08 - React Redux 2/src/features/todo/todoSlice.js
@@ -13,7 +13,21 @@ export const todoSlice = createSlice({
     name: 'todo',
     initialState,
     reducers: {
-        addTodo: (state, acttion) => {},
-        removeTodo: () => {},
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => {
+                todo.id !== action.payload
+            })
+        },
     }
 })
+
+export const { addTodo, removeTodo } = todoSlice.actions
+
+export default todoSlice.reducer
