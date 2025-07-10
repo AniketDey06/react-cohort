9ff8d1dd385492e4949d3b4906ee02c07b84b037
Aniket Dey <meaniket1025@gmail.com>
2025-07-10 12:31:48 +0530

    feat: integrate Tailwind CSS and set up authentication with Appwrite
    - Added Tailwind CSS and Vite plugin for styling.
    - Implemented authentication service using Appwrite.
    - Created Redux slice for managing authentication state.
    - Developed reusable components: Button, Input, Select, and Logo.
    - Built Header and Footer components with navigation and logout functionality.
    - Set up main application structure with loading state and user session management.
    - Added PostCard component for displaying posts with images.
    - Configured Redux store and integrated it into the application.

--- 09 - React Mega Project/README.md
+++ 09 - React Mega Project/README.md
@@ -1,5 +1,7 @@
 # React + Vite
 
+Ref - https://github.com/hiteshchoudhary/chai-aur-react/tree/main/12MegaBlog
+
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
---
+++ 09 - React Mega Project/src/components/Login.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { login as authLogin } from '../store/authSlice'
+import { Button, Input, Logo } from './index'
+import { useDispatch } from 'react-redux'
+import authService from '../appwrite/auth'
+import { useForm } from 'react-hook-form'
+
+const Login = () => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const { register, handleSubmit } = useForm()
+    const [error, setError] = useState('')
+    
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Login
