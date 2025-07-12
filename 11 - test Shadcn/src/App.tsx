12e80443acfc90fda36ddf79d4d0245eba58c53e
Aniket Dey <meaniket1025@gmail.com>
2025-07-13 01:30:45 +0530

    add

--- 11 - test Shadcn/src/App.tsx
+++ 11 - test Shadcn/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <Button>Click me</Button>
+      <Button variant='outline'>Click me {count}</Button>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
