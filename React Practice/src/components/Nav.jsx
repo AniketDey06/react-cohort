c5371c27e0758fa6fc9eab64eef62ae164b0d3d3
Aniket Dey <meaniket1025@gmail.com>
2025-08-03 00:52:36 +0530

    nav done

--- React Practice/src/components/Nav.jsx
+++ React Practice/src/components/Nav.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <nav className='text-white w-full h-15 flex items-center justify-around bg-emerald-900'>
       <h1 className='text-4xl'>Aniket</h1>
       <div className='flex gap-3 items-center'>
-        <a className='text-lg' href="">Contact</a>
-        <a className='text-lg' href="">About</a>
-        <a className='text-lg' href="">Blog</a>
-        <a className='text-lg' href="">Skills</a>
+        <Link className='text-lg' to="/">Home</Link>
+        <Link className='text-lg' to="/Contact">Contact</Link>
+        <Link className='text-lg' to="/About">About</Link>
+        <Link className='text-lg' to="/product">Product</Link>
       </div>
     </nav>
   )
